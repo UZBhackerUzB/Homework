c6ef2ef40b6d36c1a934798904600869f799cd8c
UZBhackerUzb <haydarovaferuza37@gmail.com>
2024-01-16 09:25:27 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// for, while, do while
+
+// for(let i=1;i<=10;i++){
+//     console.log("Hello")
+// }
+
+// let i = 10
+
+// while(i < 10){
+//     console.log(i)
+//     i++;
+// }
+
+// let i = 0
+
+// do{
+//     console.log(i)
+//     i++
+// }while(i<10)
+
+
+// for(let i=1; i<=1000000; i++){
+//     if(i%2 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+let oy = prompt("Oyni kiriting") 
+
+switch(oy){
+    case "Yanvar":  console.log("1-oy");break;
+    case "Fevral": console.log("2-oy");break;
+    case "Mart":  console.log("3-oy");break;
+    case "Aprel":  console.log("4-oy");break;
+    case "May": console.log("5-oy");break;
+    case "Iyun":  console.log("6-oy");break;
+    case "Iyul":  console.log("7-oy");break;
+    case "Avgust": console.log("8-oy");break;
+    case "Sentyabr":  console.log("9-oy");break;
+    case "Oktyabr":  console.log("10-oy");break;
+    case "Noyabr":  console.log("11-oy");break;
+    case "Dekabr": console.log("12-oy");break;
+    default: console.log("Bunday oy yo'q")
+}
